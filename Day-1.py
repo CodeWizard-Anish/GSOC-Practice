3317f9b2becb61e28c136aba9997d36f7ad79b01
CodeWizard-Anish <anishraj4343@gmail.com>
2025-11-19 21:24:41 +0530

    Add files via upload
    Added Day 1 Python practice file

---
+++ Day-1.py
@@ -0,0 +1,9 @@
+Name = "GSOC"
+year = 2026
+
+def greet(name, year):
+    return f"Welcome to {Name} {year}!",f"{user} Welcome back to Python!"
+
+user = input("Enter your name: ")
+print(greet(user, year))
+
